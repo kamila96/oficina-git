d662c046810efa6755fb098cf2813be5cb1e2a3f
Davi Izel <davi.sk84ever@gmail.com>
2018-02-07 21:25:43 -0400

    encapsulamento implementado

--- src/br/edu/fmf/modelo/Gato.java
+++ src/br/edu/fmf/modelo/Gato.java
@@ -8,4 +8,34 @@ public class Gato {
 	private int idade;
 
 	//métodos
+	public Gato(String cor, String nome, int idade){
+		this.cor = cor;
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
 }
